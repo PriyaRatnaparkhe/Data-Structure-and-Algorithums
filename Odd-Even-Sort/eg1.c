4833729267a2c03fed4322be9243cf2cfecc7810
Priya Ratnaparkhe <82965104+PriyaRatnaparkhe@users.noreply.github.com>
2021-09-17 17:52:01 +0530

    Add files via upload

--- Odd-Even-Sort/eg1.c
+++ Odd-Even-Sort/eg1.c
@@ -1,1 +1,48 @@
-
+#include<stdio.h>
+int main()
+{
+int g,swap=0,i,j,x[10],y;
+for(y=0;y<=9;y++)
+{
+printf("Enter a number :");
+scanf("%d",&x[y]);
+}
+while(1)
+{
+swap=0;
+i=1;
+j=i+1;
+while(i<=7)
+{
+if(x[j]<x[i])
+{
+g=x[i];
+x[i]=x[j];
+x[j]=g;
+swap++;
+}
+i+=2;
+j+=2;
+}
+i=0;
+j=i+1;
+while(i<=8)
+{
+if(x[j]<x[i])
+{
+g=x[i];
+x[i]=x[j];
+x[j]=g;
+swap++;
+}
+i+=2;
+j+=2;
+}
+if(swap==0) break;
+}
+for(y=0;y<=9;y++)
+{
+printf("%d\n",x[y]);
+}
+return 0;
+}
