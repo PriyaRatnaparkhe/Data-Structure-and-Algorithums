333f559801de9f21716ae03cd99f131da178b9b8
Priya Ratnaparkhe <82965104+PriyaRatnaparkhe@users.noreply.github.com>
2021-09-17 17:43:00 +0530

    Add files via upload

--- Cocktail-Shaker-Sort/eg1.c
+++ Cocktail-Shaker-Sort/eg1.c
@@ -1,1 +1,49 @@
-
+#include<stdio.h>
+int main()
+{
+int y,x[10],lb,ub,e,g,swapCount=0;
+for(y=0;y<=9;y++)
+{
+printf("Enter a number :");
+scanf("%d",&x[y]);
+}
+lb=0;
+ub=9;
+while(1)
+{
+swapCount=0;
+e=lb;
+while(e<ub)
+{
+if(x[e+1]<x[e])
+{
+g=x[e];
+x[e]=x[e+1];
+x[e+1]=g;
+swapCount=1;
+}
+e=e+1;
+}
+ub=ub-1;
+if(swapCount==0) break;
+e=ub;
+while(e>lb)
+{
+if(x[e]<x[e-1])
+{
+g=x[e];
+x[e]=x[e-1];
+x[e-1]=g;
+swapCount=1;
+}
+e=e-1;
+}
+if(swapCount==0) break;
+lb=lb+1;
+}
+for(y=0;y<=9;y++)
+{
+printf("%d\n",x[y]);
+}
+return 0;
+}
