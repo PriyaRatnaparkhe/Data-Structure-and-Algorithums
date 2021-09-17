adc46649b8da7643be19ad7cc9254631106b6001
Priya Ratnaparkhe <82965104+PriyaRatnaparkhe@users.noreply.github.com>
2021-09-17 17:47:15 +0530

    Add files via upload

--- GNome-Sort/eg1.c
+++ GNome-Sort/eg1.c
@@ -1,1 +1,25 @@
-
+#include<stdio.h>
+int main()
+{
+int x[10],y,j,g;
+for(y=0;y<=9;y++)
+{
+printf("Enter a number :");
+scanf("%d",&x[y]);
+}
+j=0;
+while(j<=9)
+{
+if(j==0) j=j+1;
+if(x[j]<x[j-1])
+{
+g=x[j-1];
+x[j-1]=x[j];
+x[j]=g;
+j--;
+}
+else j++;
+}
+for(y=0;y<=9;y++) printf("%d\n",x[y]);
+return 0;
+}
