a85b694ef2e7d975b3ce770b04a2ee5b8c2aa5c4
Priya Ratnaparkhe <82965104+PriyaRatnaparkhe@users.noreply.github.com>
2021-09-17 17:58:33 +0530

    Add files via upload

--- Stooge-Sort/eg1.c
+++ Stooge-Sort/eg1.c
@@ -1,1 +1,36 @@
-
+#include<stdio.h>
+void stoogeSort(int *x,int lb,int ub)
+{
+int size,g,t;
+size=(ub-lb)+1;
+if(x[lb]>x[ub])
+{
+g=x[lb];
+x[lb]=x[ub];
+x[ub]=g;
+}
+if(size>2)
+{
+t=size/3;
+stoogeSort(x,lb,ub-t);
+stoogeSort(x,lb+t,ub);
+stoogeSort(x,lb,ub-t);
+}
+}
+int main()
+{
+int x[10],y,lb,ub;
+for(y=0;y<=9;y++)
+{
+printf("Enter a number :");
+scanf("%d",&x[y]);
+}
+lb=0;
+ub=9;
+stoogeSort(x,lb,ub);
+for(y=0;y<=9;y++)
+{
+printf("%d\n",x[y]);
+}
+return 0;
+}
